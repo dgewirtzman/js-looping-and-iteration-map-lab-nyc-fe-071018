7277cdd1dd43762fb863517c6d5bbdd25d8b5ce5
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-20 20:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(driverInfo) {
   driverInfo.map((val, index, driverInfo) => {
-    console.log(driverInfo[index].name + " is from " + driverInfo[index].home )
+    console.log(driverInfo[index].name + " is from " + driverInfo[index].hometown )
   })
 }
