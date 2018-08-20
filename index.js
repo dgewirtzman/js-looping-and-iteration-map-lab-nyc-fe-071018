734252d89366fc78b5757bcae36685aa39306652
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-20 19:51:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(driverList) {
+  return driverList.map(driver => driver.toLowerCase())
+}
+
+function nameToAttributes(drivers) {
+  
+}
+
+function attributesToPhrase(driverList) {
+  console.log(driverList.map(driver =>  is from {$hometown}`})
+}
